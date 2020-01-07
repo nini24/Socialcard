3552ee202783545f36ebf161459a9bb49d9abffb
nini24 <niniakintelure.com>
2020-01-07 18:34:24 +0100

    Updates

---
+++ precache-manifest.152b860f720dede09960e724c788eb37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5c465869be626a67658628132f5265",
+    "url": "/Socialcard/index.html"
+  },
+  {
+    "revision": "4449fc3cc33274bc6b50",
+    "url": "/Socialcard/static/css/main.e43c6dcf.chunk.css"
+  },
+  {
+    "revision": "4e5fdb7b251736966952",
+    "url": "/Socialcard/static/js/2.e4aec962.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Socialcard/static/js/2.e4aec962.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4449fc3cc33274bc6b50",
+    "url": "/Socialcard/static/js/main.a680bd37.chunk.js"
+  },
+  {
+    "revision": "bf8f411db99139924765",
+    "url": "/Socialcard/static/js/runtime-main.e8259b75.js"
+  },
+  {
+    "revision": "20213d8e954e8637b350fcf031ff80e0",
+    "url": "/Socialcard/static/media/mat.20213d8e.jpg"
+  }
+]);
